5c0db047ce6d3daca457a617f53506df05a0eecd
hnunezz <nunesdealmeidahenrique@gmail.com>
2024-07-01 22:59:17 -0300

    feat: init new payment view

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,24 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+  ],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.scss'
 })
-export class PaymentComponent implements OnInit{
+export class PaymentComponent implements OnInit {
   private router = inject(Router);
 
   ngOnInit(): void {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
-  })
+    })
   }
 
   navigateTo() {
     this.router.navigate(['/']);
-}
+  }
 }
